5748525c9d368977e0faa0240eaf8531931044b8
Alexander Noskov <a13x.exe@yandex.ru>
2025-10-02 17:11:52 +0600

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,68 @@
+package org.example;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    }
+
+    //1. Средний элемент
+    public static void middleElement() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] strNums = reader.readLine().split(" ");
+
+        int[] intNums = new int[strNums.length];
+
+        for (int i = 0; i < strNums.length; i++) {
+            intNums[i] = Integer.parseInt(strNums[i]);
+        }
+        Arrays.sort(intNums);
+        writer.write(String.valueOf(intNums[1]));
+
+        reader.close();
+        writer.close();
+    }
+
+    //2. Самый дешевый путь
+    public static void cheapestWay() throws IOException {
+        int n, m;
+        int[][] matrix;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = reader.readLine();
+            String[] strNums = line.split(" ");
+
+            n = Integer.parseInt(strNums[0]);
+            m = Integer.parseInt(strNums[1]);
+
+            matrix = new int[n][m];
+
+            for (int j = 0; j < n; j++) {
+                line = reader.readLine();
+                strNums = line.split(" ");
+                for (int i = 0; i < m; i++) {
+                    matrix[j][i] = Integer.parseInt(strNums[i]);
+                }
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            matrix[i][0] += matrix[i - 1][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            matrix[0][i] += matrix[0][i - 1];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                matrix[i][j] += Math.min(matrix[i - 1][j], matrix[i][j - 1]);
+            }
+        }
+
+        System.out.println(matrix[n-1][m-1]);
+    }
+}
