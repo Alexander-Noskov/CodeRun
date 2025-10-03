1bde32aeaf9f589f10a0ba96dbbcb0b6c571aaa7
Alexander Noskov <a13x.exe@yandex.ru>
2025-10-03 23:50:25 +0600

    3. Вывести маршрут максимальной стоимости

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 [1. Средний элемент](https://github.com/Alexander-Noskov/CodeRun/blob/main/src/main/java/org/example/Main.java#L10-L27)
 
 [2. Самый дешевый путь](https://github.com/Alexander-Noskov/CodeRun/blob/main/src/main/java/org/example/Main.java#L29-L68)
+
+[3. Вывести маршрут максимальной стоимости](https://github.com/Alexander-Noskov/CodeRun/blob/main/src/main/java/org/example/Main.java#L70-L121)
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -65,4 +65,57 @@ public class Main {
 
         System.out.println(matrix[n-1][m-1]);
     }
+
+    //3. Вывести маршрут максимальной стоимости
+    public static void maxCostRoad() throws IOException {
+        int n, m;
+        int[][] matrix;
+        String[][] path;
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = reader.readLine();
+            String[] strNums = line.split(" ");
+
+            n = Integer.parseInt(strNums[0]);
+            m = Integer.parseInt(strNums[1]);
+
+            matrix = new int[n][m];
+            path = new String[n][m];
+
+            for (int j = 0; j < n; j++) {
+                line = reader.readLine();
+                strNums = line.split(" ");
+                for (int i = 0; i < m; i++) {
+                    matrix[j][i] = Integer.parseInt(strNums[i]);
+                }
+            }
+        }
+        path[0][0] = "";
+
+        for (int i = 1; i < n; i++) {
+            matrix[i][0] += matrix[i - 1][0];
+            path[i][0] = path[i - 1][0] + "D ";
+        }
+
+        for (int i = 1; i < m; i++) {
+            matrix[0][i] += matrix[0][i - 1];
+            path[0][i] = path[0][i - 1] + "R ";
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i - 1][j] > matrix[i][j - 1]) {
+                    matrix[i][j] += matrix[i - 1][j];
+                    path[i][j] = path[i - 1][j] + "D ";
+                } else {
+                    matrix[i][j] += matrix[i][j - 1];
+                    path[i][j] = path[i][j - 1] + "R ";
+                }
+            }
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            writer.write(matrix[n-1][m-1] + "\n" + path[n-1][m-1]);
+        }
+    }
 }
